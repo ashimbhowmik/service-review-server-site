edcc53fa32b87d92dae15e0793bf1cff72c79978
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-11-08 23:09:39 +0600

    middleware set

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mvrzlyk.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 app.get("/", (req, res) => {
   res.send("Service project start");
 });
