fb0136244d4e9953e1f7c43518984834e48e4e22
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-11-08 23:03:07 +0600

    server start

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Service project start");
+});
+
+app.listen(port, () => {
+  console.log(`Service project start on ${port}`);
+});
